c00812d7d3d44b5013f6a608848595d59a68c905
kiboi-dot <kiboigriffin@gmail.com>
2025-04-03 12:38:16 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID |XPLOADER-BOT:~ajBAlDTB#LV7vnqmyFRClwRYtHLNLx2xdPJfsWGuHZqrlBCb5Uz8| '' 
 // You know the drill. Put your CypherX session id here. Should look like: XPLOADER-BOT:~ (don't forget that, it's crucial!)
 
 //--------------------[ BOT NAME ]----------------------//
@@ -27,9 +27,9 @@ global.SESSION_ID = process.env.SESSION_ID || ''
 global.botname = process.env.BOT_NAME || 'CypherX' 
 // Pick a name for the bot. Default is "CypherX" — but who doesn’t love a cool bot name?
 
-//-----------------[ OWNER NUMBER ]------------------//
+//---------KIBOI--------[ OWNER NUMBER ]--------------/25420168236/
 
-global.ownernumber = process.env.OWNER_NUMBER || '254754783972' 
+global.ownernumber = process.env.OWNER_NUMBER || '254720168236' 
 // Your personal contact number. If you want to call Tylor, this is the number. 👀
 
 //-----------------[ OWNER NAME ]------------------//
